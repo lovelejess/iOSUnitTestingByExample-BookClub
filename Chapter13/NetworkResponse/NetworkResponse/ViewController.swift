ff0b89bf5bba0167d0ad2e2749f45eec73f20e6b
Quinne Farenwald <quinne.farenwald@slalom.com>
2021-01-04 20:02:12 -0700

    Parse the response

--- Chapter13/NetworkResponse/NetworkResponse/ViewController.swift
+++ Chapter13/NetworkResponse/NetworkResponse/ViewController.swift
@@ -11,9 +11,26 @@ protocol URLSessionProtocol {
     func dataTask(with request: URLRequest, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTask
 }
 
+struct Search: Decodable {
+    let results: [SearchResult]
+}
+
+struct SearchResult: Decodable, Equatable {
+    let artistName: String
+    let trackName: String
+    let averageUserRating: Float
+    let genres: [String]
+}
+
 class ViewController: UIViewController {
     private var dataTask: URLSessionDataTask?
     var session: URLSessionProtocol = URLSession.shared
+    
+    private(set) var results: [SearchResult] = [] {
+        didSet {
+            print(results)
+        }
+    }
     
     @IBOutlet private(set) var button: UIButton!
     
@@ -27,20 +44,45 @@ class ViewController: UIViewController {
         let request = URLRequest(url: url)
         dataTask = session.dataTask(with: request) {
             [weak self] (data: Data?, response: URLResponse?, error: Error?) -> Void in
-                guard let self = self else { return }
-                let decoded = String(data: data ?? Data(), encoding: .utf8)
-                print("response: \(String(describing: response))")
-                print("data: \(String(describing: decoded))")
-                print("error: \(String(describing: error))")
+            guard let self = self else { return }
+            
+            var decoded: Search?
+            var errorMessage: String?
+            if let error = error {
+                errorMessage = error.localizedDescription
+            } else if let response = response as? HTTPURLResponse, response.statusCode != 200 {
+                errorMessage = "Response: " + HTTPURLResponse.localizedString(forStatusCode: response.statusCode)
+            } else if let data = data {
+                do {
+                    decoded = try JSONDecoder().decode(Search.self, from: data)
+                } catch {
+                    errorMessage = error.localizedDescription
+                }
+            }
             
             DispatchQueue.main.async { [weak self] in
                 guard let self = self else { return }
+                if let decoded = decoded {
+                    self.results = decoded.results
+                }
+                if let errorMessage = errorMessage {
+                    self.showError(errorMessage)
+                }
                 self.dataTask = nil
                 self.button.isEnabled = true
             }
         }
         button.isEnabled = false
         dataTask?.resume()
+    }
+    
+    private func showError(_ message: String) {
+        let title = "Network problem"
+        print("\(title): \(message)")
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default)
+        alert.preferredAction = okAction
+        present(alert, animated: true)
     }
     
     @IBAction private func buttonTapped() {
