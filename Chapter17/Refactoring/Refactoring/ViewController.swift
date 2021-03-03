7f8a5e8acb469e903d6d2bbc0f6d5d2e9ac3b895
Quinne Farenwald <q.farenwald@gmail.com>
2021-03-02 21:40:09 -0700

    Create chp 17 files and set up storyboards

---
+++ Chapter17/Refactoring/Refactoring/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Refactoring
+//
+//  Created by Quinne Farenwald on 3/2/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
