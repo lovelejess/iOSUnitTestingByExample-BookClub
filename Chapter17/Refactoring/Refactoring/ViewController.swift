d6ac20c87739f9fa631b8b5901ade30b333c6d41
Quinne Farenwald <q.farenwald@gmail.com>
2021-03-02 21:49:31 -0700

    Edit VC

--- Chapter17/Refactoring/Refactoring/ViewController.swift
+++ Chapter17/Refactoring/Refactoring/ViewController.swift
@@ -7,13 +7,14 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+final class ViewController: UIViewController {
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        super.prepare(for: segue, sender: sender)
+        if segue.identifier == "changePassword" {
+            let changePasswordVC = segue.destination
+                    as? ChangePasswordViewController
+            changePasswordVC?.securityToken = "TOKEN"
+        }
     }
-
-
 }
 
